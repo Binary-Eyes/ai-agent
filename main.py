07fd6ab3314d030bc1586a2e5b07c01c00640f89
amir_barak <binary.eyes.studio@gmail.com>
2025-06-18 23:03:43 +0300

    some minor cleanup

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
+    if len(sys.argv) < 2:
+        raise Exception("prompt not detected in cli args")
+    
+    model = "gemini-2.0-flash-001"
+    prompt = sys.argv[1]
+    
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001", 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
+    response = client.models.generate_content(model=model, contents=prompt)
 
+    print("------------------------------------------------------------------------------")
     print(response.text)
+    print("------------------------------------------------------------------------------")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")    
 
