6ef3724240f549ee13b3e7ddc49713a5738b5e6d
amir_barak <binary.eyes.studio@gmail.com>
2025-06-20 09:03:02 +0300

    implemented the get files

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,13 +2,28 @@ import os
 
 def get_files_info(working_directory, directory=None):
     try:
-        path = os.path.abspath(directory)
-        if not path.startswith(working_directory):
+        root = os.path.abspath(working_directory)
+        path = os.path.join(root, directory)
+        if not os.path.abspath(path).startswith(root):
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-        
-        if not os.path.isdir(directory):
+
+        if not os.path.isdir(path):
             return f'Error: "{directory}" is not a directory'
+        
+        items = []
+        for item in os.listdir(path):
+            item_path = os.path.join(path, item)
+            is_dir = False
+            
+            if os.path.isdir(item_path):
+                is_dir = True
+
+            size = os.path.getsize(item_path)
+            items.append(f"- {item}: file_size={size} bytes, is_dir={is_dir}")
+
+        return "\n".join(items)
+
     except Exception as e:
-        return e
+        return f"Error: {e}"
 
 
