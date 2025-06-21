fa6df1bcf12d59a0ac596d6539f99c2eb76a2089
amir_barak <binary.eyes.studio@gmail.com>
2025-06-21 12:19:52 +0300

    testing function to write files

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,2 +1,21 @@
+import os
+
 def write_file(working_directory, file_path, content):
-    pass
+    try:
+        root = os.path.abspath(working_directory)
+        path = os.path.join(root, file_path)
+        if not os.path.abspath(path).startswith(root):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+        if not os.path.isfile(path):
+            file_dir = os.path.dirname(path)
+            os.mkdir(file_dir)
+
+        with open(path, "w") as f:
+            f.write(content)
+
+        return f'Successfully wrote to "{path}" ({len(content)} characters written)'
+
+    except Exception as e:
+        return f"Error: {e}"      
+        
