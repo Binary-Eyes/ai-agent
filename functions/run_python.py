f7cb288f65ffbcad23b92eb52e95f6fa5a35e5fe
amir_barak <binary.eyes.studio@gmail.com>
2025-06-21 21:48:36 +0300

    minor updates

--- functions/run_python.py
+++ functions/run_python.py
@@ -7,13 +7,13 @@ def run_python_file(working_directory, file_path):
         root = os.path.abspath(working_directory)
         path = os.path.join(root, file_path)
         if not os.path.abspath(path).startswith(root):
-            return f'Error: Cannot read "{path}" as it is outside the permitted working directory'
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
         
         if not os.path.isfile(path):
-            return f'Error: File "{path}" not found.'
+            return f'Error: File "{file_path}" not found.'
         
         if not pathlib.Path(path).suffix == ".py":
-            return f'Error: "{path}" is not a Python file.'
+            return f'Error: "{file_path}" is not a Python file.'
         
         completed = subprocess.run(
             args=["python3", path], 
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     if len(sys.argv) < 2:
         raise Exception("prompt not detected in cli args")
     
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
     model = "gemini-2.0-flash-001"
     user_prompt = sys.argv[1]
     messages = [
@@ -17,9 +18,12 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model=model, contents=messages)
+    response = client.models.generate_content(
+        model=model, 
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt))
+    
     print(response.text)
-
     if is_verbose_enabled(sys.argv):
         print_verbose(user_prompt, response)
 
