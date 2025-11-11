34c68cacc8c85df8aaa342baaa8859a7bc162e24
amir_barak <binary.eyes.studio@gmail.com>
2025-11-11 09:24:03 +0200

    refactored main to use functions

--- main.py
+++ main.py
@@ -3,17 +3,32 @@ import sys
 from google import genai
 from dotenv import load_dotenv
 
-if len(sys.argv) != 2:
-    print("WARNING: no prompt supplied to agent")
-    sys.exit(1)
+def main(args):
+    if len(args) < 2:
+        print("WARNING: no prompt supplied to agent")
+        sys.exit(1)
 
-load_dotenv()
-prompt = sys.argv[1]
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-reply = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=prompt)
-print(reply.text)
-print(f"Prompt tokens: {reply.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {reply.usage_metadata.candidates_token_count}")
+    verbose_enabled = False
+    for i in range(2, len(args)):
+        if sys.argv[i] != "--verbose":
+            continue
+        verbose_enabled = True
+        break
+
+    load_dotenv()
+    prompt = sys.argv[1]
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    reply = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=prompt)
+
+    print(reply.text)
+    print(f"Prompt tokens: {reply.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {reply.usage_metadata.candidates_token_count}")
+    
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+
